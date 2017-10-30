43970001dc02580fc5a29c8b5a5c3b3f06faa3fe
Blake Johnson <brjohnson@umail.ucsb.edu>
2017-10-29 20:02:03 -0700

    added updated version of SimpleGui1 to my src directory - done with Step 5

---
+++ src/edu/ucsb/cs56/drawings/brjohnson61/SimpleGui1.java
@@ -0,0 +1,34 @@
+package edu.ucsb.cs56.drawings.brjohnson61;
+
+import javax.swing.*;
+
+/** SimpleGui1 comes from Head First Java 2nd Edition p. 355.
+    It illustrates a simple GUI with a Button that doesn't do anything yet.
+    
+    @author Head First Java, 2nd Edition p. 355
+    @author P. Conrad (who only typed it in and added the Javadoc comments)
+    @author Blake Johnson CSIL: brjohnson61 GitHubID: brjohnson61
+    @version CS56, Spring 2013, UCSB
+*/
+
+public class SimpleGui1 {
+    
+    /** main method to fire up a JFrame on the screen
+	@param args not used
+    */
+    
+    public static void main (String[] args) {
+	JFrame frame = new JFrame() ;
+	
+	JButton button = new JButton("Click me to see magic happen");
+	
+	java.awt.Color myColor = new java.awt.Color(000,102,000);   // R, G, B values.
+	button.setBackground(myColor);
+	button.setOpaque(true);
+	
+	frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
+	frame. getContentPane() . add(button) ;
+	frame. setSize(300,300) ;
+	frame. setVisible(true) ;
+    }
+}
